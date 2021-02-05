9c575d67dcc955b3b61afdd51b038e98bc793bb9
HsnPierre <pierre.hsn@gmail.com>
2021-02-05 15:25:41 +0100

    codacy filter_input_array

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use App\Autoloader;
 use App\Core\Main;
 
 define('ROOT', dirname(__DIR__));
-define('HOST', "http://".$_SERVER['SERVER_NAME']."/");
+define('HOST', "http://localhost/");
 
 require_once ROOT.'/Autoloader.php';
 Autoloader::register();
